29a87625d7dae093eeba3f5d5053141073bcef23
Peter Duan <peter@getpocketbook.com>
2014-08-07 16:44:50 +1000

    new pulgin for appirator-android

--- app/src/main/java/com/sbstrm/appirater/Appirater.java
+++ app/src/main/java/com/sbstrm/appirater/Appirater.java
@@ -1,11 +1,15 @@
 package com.sbstrm.appirater;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.Build;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.LayoutInflater;
@@ -33,6 +37,7 @@ import android.widget.TextView;
 public class Appirater {
 
 	private static final String PREF_LAUNCH_COUNT = "launch_count";
+	private static final String PREF_EVENT_COUNT = "event_count";
 	private static final String PREF_RATE_CLICKED = "rateclicked";
 	private static final String PREF_DONT_SHOW = "dontshow";
 	private static final String PREF_DATE_REMINDER_PRESSED = "date_reminder_pressed";
@@ -47,12 +52,16 @@ public class Appirater {
         SharedPreferences.Editor editor = prefs.edit();
         
         if(testMode){
-        	showRateDialog(mContext, editor);
+        	//showRateDialog(mContext, editor);
+            showLoveDialog(mContext, editor);
         	return;
         }
         
         // Increment launch counter
         long launch_count = prefs.getLong(PREF_LAUNCH_COUNT, 0);
+
+        // Get events counter
+        long event_count = prefs.getLong(PREF_EVENT_COUNT, 0);
 
         // Get date of first launch
         long date_firstLaunch = prefs.getLong(PREF_DATE_FIRST_LAUNCHED, 0);
@@ -63,8 +72,10 @@ public class Appirater {
         try{
 	        int appVersionCode = mContext.getPackageManager().getPackageInfo(mContext.getPackageName(), 0).versionCode;
 	        if(prefs.getInt(PREF_APP_VERSION_CODE, 0)  != appVersionCode){
-	        	//Reset the launch count to help assure users are rating based on the latest version. 
-	        	launch_count = 0;
+		        //Reset the launch and event counters to help assure users are rating based on the latest version. 
+		        launch_count = 0;
+		        event_count = 0;
+		        editor.putLong(PREF_EVENT_COUNT, event_count);
 	        }
 	        editor.putInt(PREF_APP_VERSION_CODE, appVersionCode);
         }catch(Exception e){
@@ -79,16 +90,18 @@ public class Appirater {
             editor.putLong(PREF_DATE_FIRST_LAUNCHED, date_firstLaunch);
         }
         
-        // Wait at least n days before opening
+        // Wait at least n days or m events before opening
         if (launch_count >= mContext.getResources().getInteger(R.integer.appirator_launches_until_prompt)) {
 			long millisecondsToWait = mContext.getResources().getInteger(R.integer.appirator_days_until_prompt) * 24 * 60 * 60 * 1000L;			
-			if (System.currentTimeMillis() >= (date_firstLaunch + millisecondsToWait)) {
+			if (System.currentTimeMillis() >= (date_firstLaunch + millisecondsToWait) || event_count >= mContext.getResources().getInteger(R.integer.appirator_events_until_prompt)) {
 				if(date_reminder_pressed == 0){
-					showRateDialog(mContext, editor);
+					//showRateDialog(mContext, editor);
+                    showLoveDialog(mContext, editor);
 				}else{
 					long remindMillisecondsToWait = mContext.getResources().getInteger(R.integer.appirator_days_before_reminding) * 24 * 60 * 60 * 1000L;
 					if(System.currentTimeMillis() >= (remindMillisecondsToWait + date_reminder_pressed)){
-						showRateDialog(mContext, editor);
+						//showRateDialog(mContext, editor);
+                        showLoveDialog(mContext, editor);
 					}
 				}
 			}
@@ -96,13 +109,39 @@ public class Appirater {
         
         editor.commit();
     }   
-    
+
+    public static void rateApp(Context mContext)
+    {
+        SharedPreferences prefs = mContext.getSharedPreferences(mContext.getPackageName()+".appirater", 0);
+        SharedPreferences.Editor editor = prefs.edit();
+        rateApp(mContext, editor);
+    }
+
+	@TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    public static void significantEvent(Context mContext) {
+        boolean testMode = mContext.getResources().getBoolean(R.bool.appirator_test_mode);
+        SharedPreferences prefs = mContext.getSharedPreferences(mContext.getPackageName()+".appirater", 0);
+        if(!testMode && (prefs.getBoolean(PREF_DONT_SHOW, false) || prefs.getBoolean(PREF_RATE_CLICKED, false))) {return;}
+
+        long event_count = prefs.getLong(PREF_EVENT_COUNT, 0);
+        event_count++;
+        prefs.edit().putLong(PREF_EVENT_COUNT, event_count).apply();
+    }
+
+    private static void rateApp(Context mContext, final SharedPreferences.Editor editor) {
+        mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(String.format(mContext.getString(R.string.appirator_market_url), mContext.getPackageName()))));
+        if (editor != null) {
+            editor.putBoolean(PREF_RATE_CLICKED, true);
+            editor.commit();
+        }
+    }
+
 	@SuppressLint("NewApi")
 	private static void showRateDialog(final Context mContext, final SharedPreferences.Editor editor) {
     	String appName = mContext.getString(R.string.appirator_app_title);
         final Dialog dialog = new Dialog(mContext);
         
-        if (android.os.Build.VERSION.RELEASE.startsWith("1.") || android.os.Build.VERSION.RELEASE.startsWith("2.0") || android.os.Build.VERSION.RELEASE.startsWith("2.1")){
+        if (Build.VERSION.RELEASE.startsWith("1.") || Build.VERSION.RELEASE.startsWith("2.0") || Build.VERSION.RELEASE.startsWith("2.1")){
         	//No dialog title on pre-froyo devices
         	dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         }else if(mContext.getResources().getDisplayMetrics().densityDpi == DisplayMetrics.DENSITY_LOW || mContext.getResources().getDisplayMetrics().densityDpi == DisplayMetrics.DENSITY_MEDIUM){
@@ -126,11 +165,7 @@ public class Appirater {
         rateButton.setText(String.format(mContext.getString(R.string.rate), appName));
         rateButton.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-                mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(String.format(mContext.getString(R.string.appirator_market_url), mContext.getPackageName()))));
-                if (editor != null) {
-                    editor.putBoolean(PREF_RATE_CLICKED, true);
-                    editor.commit();
-                }
+                rateApp(mContext, editor);
                 dialog.dismiss();
             }
         });
@@ -161,5 +196,49 @@ public class Appirater {
 
         dialog.setContentView(layout);        
         dialog.show();        
+    }
+
+    private static void showLoveDialog(final Context mContext, final SharedPreferences.Editor editor) {
+
+        final Dialog dialog = new Dialog(mContext);
+        LinearLayout layout = (LinearLayout) LayoutInflater.from(mContext).inflate(R.layout.loveapp, null);
+
+        TextView textView = (TextView)layout.findViewById(R.id.love_dialog_message);
+        Button yesButton = (Button)layout.findViewById(R.id.love_dialog_yes);
+        Button noButton = (Button)layout.findViewById(R.id.love_dialog_no);
+
+        textView.setText(String.format(mContext.getString(R.string.love_dialog_content)));
+        yesButton.setText(String.format(mContext.getString(R.string.love_dialog_yes)));
+        noButton.setText(String.format(mContext.getString(R.string.love_dialog_no)));
+
+        yesButton.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                showRateDialog(mContext, editor);
+            }
+        });
+
+        noButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editor != null) {
+                    editor.putBoolean(PREF_DONT_SHOW, true);
+                    editor.commit();
+                }
+                dialog.dismiss();
+
+                Intent intent = new Intent();
+                intent.setAction("com.sbstrm.appirater.Appirater");
+                intent.putExtra("HATE_APP", true);
+                v.getContext().sendBroadcast(intent);
+            }
+        });
+
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(layout, dialog.getWindow().getAttributes());
+        dialog.show();
     }
 }
